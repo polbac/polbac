41e1df0b1ac8c062fbfbe6f15ea519a2ca78fc5f
polbac <polbac@gmail.com>
2021-06-15 20:21:22 -0300

    .

--- pages/tree.js
+++ pages/tree.js
@@ -1,8 +1,13 @@
+import Head from 'next/head'
 
 
 export default () => {
     return (
         <>
+        <Head>
+        <title>@polbac</title>
+        
+        </Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Bentham&display=swap" rel="stylesheet" />
 
@@ -12,7 +17,7 @@ export default () => {
             <marquee>@polbac</marquee>        
                 <article>
                     <p>INFRAMUNDO</p>
-                    <a href="https://inframundo.strlac.xyz/">WEBSITE</a>
+                    <a href="https://inframundo.strlac.xyz/">WEBSITE (only desktop)</a>
                     <a href="https://soundcloud.com/strlac-records/sets/inframundo">SOUNDCLOUD</a>
                     <a href="https://strlacrecords.bandcamp.com/album/inframundo">BANDCAMP</a>
                 </article>
