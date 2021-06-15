87e6b6a60c65f496fef2a9e092a9caad560b1a75
polbac <polbac@gmail.com>
2021-06-15 20:17:17 -0300

    .

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../tree.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
---
+++ pages/tree.js
@@ -0,0 +1,23 @@
+
+
+export default () => {
+    return (
+        <>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Bentham&display=swap" rel="stylesheet" />
+
+        <main className="tree">
+        
+            <div className="area">
+            <marquee>@polbac</marquee>        
+                <article>
+                    <p>INFRAMUNDO</p>
+                    <a href="https://inframundo.strlac.xyz/">WEBSITE</a>
+                    <a href="https://soundcloud.com/strlac-records/sets/inframundo">SOUNDCLOUD</a>
+                    <a href="https://strlacrecords.bandcamp.com/album/inframundo">BANDCAMP</a>
+                </article>
+            </div>
+        </main>
+        </>
+    )
+}
