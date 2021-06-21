5773b60ae2004d4a103b81e2a27034fcdaeb5a2a
polbac <polbac@gmail.com>
2021-06-21 20:13:02 -0300

    chan

--- pages/tree.js
+++ pages/tree.js
@@ -9,14 +9,17 @@ export default () => {
         
         </Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Bentham&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Fraunces:wght@300&display=swap" rel="stylesheet"/>
 
         <main className="tree">
         
             <div className="area">
-            <marquee>@polbac</marquee>        
+                <div className="head">
+                    @polbac
+                </div>
                 <article>
                     <p>INFRAMUNDO</p>
+                    <a href="https://open.spotify.com/album/5wWDrC80JpW4NIFAtBtC6r?si=CBl0C70NT6OP80zSATfH0g&dl_branch=1">SPOTIFY</a>
                     <a href="https://inframundo.strlac.xyz/">WEBSITE (only desktop)</a>
                     <a href="https://soundcloud.com/strlac-records/sets/inframundo">SOUNDCLOUD</a>
                     <a href="https://strlacrecords.bandcamp.com/album/inframundo">BANDCAMP</a>
