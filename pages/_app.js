f7d165ce546cf982f49445cf94cd4f78c80ab988
polbac <polbac@gmail.com>
2025-12-30 16:28:37 -0300

    .

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,9 @@ export default function MyApp({ Component, pageProps }) {
 
     <Head>
       <title>polbac</title>
-
+      <meta name="description" content="polbac" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.gif" />
 
     </Head>
     <Header />
